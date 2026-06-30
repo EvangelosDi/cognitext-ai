91f7ef5f7f10c8ed78aa88c62fedf04878b28a7c
Vangelis_Diaskoufis <vagdiask@gmail.com>
2026-06-30 23:20:01 +0300

    Connect local Ollama LLM for RAG answers

--- backend/app/services/llm_service.py
+++ backend/app/services/llm_service.py
@@ -1,7 +1,24 @@
+import ollama
+
+
+client = ollama.Client(
+    host="http://host.docker.internal:11434"
+)
+
+
 def generate_answer_from_context(
     prompt: str,
 ):
+    response = client.chat(
+        model="llama3.2:3b",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+    )
+
     return {
-        "answer": "LLM connection not implemented yet.",
-        "prompt_preview": prompt[:1000],
+        "answer": response["message"]["content"]
     }
